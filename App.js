7863881e211f850d56d8db4ebb910b71741cacc9
Ivan Murao <132795911+ivanmurao@users.noreply.github.com>
2023-10-24 22:57:14 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
